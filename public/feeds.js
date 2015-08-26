afccb5e25ea64d95fbe18e9562b964aa0df71679
李兴钢 <lishinko@qq.com>
2015-08-26 23:23:43 +0800

    feeds load ok

--- public/feeds.js
+++ public/feeds.js
@@ -1,10 +1,25 @@
 function removeFeed(ele){
-	var feed = $(ele).parent().text();
+	var feed = $(ele).siblings("span").text();
 	alert(feed);
 }
+function addFeed(link){
+	var element = '<li class="list-group-item"><span name="feed">' + link + '</span><a href="#"><span class="text-right">删除</span></a></li>';
+	$("#feed-list").append(element);
+}
+function loadFeeds(url){
+	$.get(url, function(data, status) {
+		var json = JSON.parse(data);
+		for (var i = 0;  i < json.length; i++) {
+			var link = json[i];
+			addFeed(link);
+		}
+	});
+}
+
 $(document).ready(function() {
 	$("#feeds").click(function() {
-		$("#feed-list").toggleClass("hide")
+		$("#feed-list").toggleClass("hide");
+		loadFeeds('/feeds');
 	});
 	$("#feed-list > li > a").click(function(){
 		removeFeed(this);
